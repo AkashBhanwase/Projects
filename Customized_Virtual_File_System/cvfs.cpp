6a1cd7871c76c63699cfb4b4585ff17fdcdd7278
AkashBhanwase <141357282+AkashBhanwase@users.noreply.github.com>
2024-03-12 18:25:27 +0530

    Add files via upload

---
+++ Customized_Virtual_File_System/cvfs.cpp
@@ -0,0 +1,320 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<iostream>
+
+#define MAXINODE 50
+
+#define READ 1
+#define WRITE 2
+
+#define MAXFILESIZE 2048
+
+#define REGULAR 1
+#define SPECIAL 2
+
+#define START 0
+#define CURRENT 1
+#define END 2
+
+typedef struct superblock
+{
+    int TotalInodes;
+    int FreeInode;
+}SUPERBLOCK, *PSUPERBLOCK;
+
+typedef struct inode
+{
+    char FileName[50];
+    int InodeNumber;
+    int FileSize;
+    int FileActualSize;
+    int FileType;
+    char *Buffer;
+    int LinkCount;
+    int ReferenceCount;
+    int permission;   // 1 2 3
+    struct inode *next;
+}INODE,*PINODE,**PPINODE;
+
+typedef struct filetable
+{
+    int readoffset;
+    int writeoffset;
+    int count;
+    int mode;  // 1 2 3 
+    PINODE ptrinode;
+}FILETABLE,*PFILETABLE;
+
+typedef struct ufdt
+{
+    PFILETABLE ptrfiletable;
+}UFDT;
+  
+UFDT UFDTArr[50];
+SUPERBLOCK SUPERBLOCKobj;
+PINODE head = NULL;
+
+void man(char *name)
+{
+    if(name == NULL)
+    {
+        return;
+    }
+    
+    if(strcmp(name,"create") == 0)
+    {
+        printf("Description : Used to create new regular file\n");
+        printf("Usage : create File_name Permission\n");
+    }
+    else if(strcmp(name, "read") == 0)
+    {
+        printf("Description : Used to read data from regular file\n");
+        printf("Usage : read File_name No_Of_Bytes_To_Read\n");
+    }
+    else if(strcmp(name,"write") == 0)
+    {
+        printf("Description : Used to write into regular file\n");
+        printf("Usage : write File_name\n After this enter the data that we want to write\n");
+    }
+    else if(strcmp(name, "ls") == 0)
+    {
+        printf("Description  : Used to list all information of files\n");
+        printf("Usage : ls\n");
+    }
+    else if(strcmp(name, "stat") == 0)
+    {
+        printf("Description : Used to display information of file\n");
+        printf("Usage : stat File_name\n");
+    }
+    else if(strcmp(name,"fstat") == 0)
+    {
+        printf("Description : Used to display information of file\n");
+        printf("Usage : stat File_Descriptor\n");
+    }
+    else if(strcmp(name,"truncate") == 0)
+    {
+        printf("Description : Used to remove data from file\n");
+        printf("Uasge : truncate File_name\n");
+    }
+    else if(strcmp(name,"open") == 0)
+    {
+        printf("Description : Used to open existing file\n");
+        printf("Usage  :open File_name mode \n");
+    }
+    else if(strcmp(name,"close") == 0)
+    {
+        printf("Description : Used to close opened file \n");
+        printf("Usage : close File_name\n");
+    }
+    else if(strcmp(name,"closeall") == 0)
+    {
+        printf("Description : Used to close all opened file\n");
+        printf("Usage : closeall\n");
+    }
+    else if(strcmp(name,"lseek") == 0)
+    {
+        printf("Description : Used to change file offset \n");
+        printf("Usage : lseek File_Name ChangeInOffset StartPoint\n");
+    }
+    else if(strcmp(name,"rm") == 0)
+    {
+        printf("Description : Used to delete the file \n");
+        printf("Usage : rm File_Name\n");
+    }
+    else
+    {
+        printf("ERROR : No manual entry available . \n");
+    }
+}
+
+void DisplayHelp()
+{
+    printf("ls : To List out all files\n");
+    printf("clear : To clear console\n");
+    printf("open : To open the file \n");
+    printf("close : To close the file \n");
+    printf("closeall : To close all opened files \n");
+    printf("read : To read the contents from file \n");
+    printf("write : To Write contents into file \n");
+    printf("exit : To Terminate file system \n");
+    printf("stat : To Display information of file using name \n");
+    printf("fstat : To Display information of file using file descriptor \n");
+    printf("truncate : To Remove all data from file \n");
+    printf("rm : To Delete the file \n");
+}
+
+int GetFDFromName(char *name)
+{
+    int i = 0;
+
+    while(i < 50)
+    {
+        if(UFDTArr[i].ptrfiletable != NULL)
+        {
+            if(strcmp((UFDTArr[i].ptrfiletable -> ptrinode -> FileName), name) == 0)
+            {
+                break;
+            }
+        }
+        i++;
+    }
+    
+    if(i == 50)
+    {
+        return -1;
+    }
+    else
+    {
+        return i;
+    }
+}
+
+PINODE Get_Inode(char *name)
+{
+    PINODE temp = head;
+    int i = 0;
+
+    if(name == NULL)
+    {
+        return NULL;
+    }
+
+    while(temp != NULL)
+    {
+        if(strcmp(name,temp -> FileName) == 0)
+        {
+            break;
+        }
+        temp = temp -> next;
+    }
+    return temp;
+}
+
+void CreateDILB()
+{
+    int i = 1;
+    PINODE newn = NULL;
+    PINODE temp = head;
+
+    while(i <= MAXINODE)
+    {
+            newn = (PINODE)malloc(sizeof(INODE));
+
+        newn -> LinkCount = 0;
+        newn -> ReferenceCount = 0;
+        newn -> FileType = 0;
+        newn -> FileSize = 0;
+
+            newn -> Buffer = NULL;
+            newn -> next = NULL;
+
+        newn -> InodeNumber = i;
+            
+            if(temp == NULL)
+            {
+                head = newn;
+                temp = head;
+            }
+            else
+            {
+                temp -> next = newn;
+                temp = temp -> next;
+            }
+            i++;
+    }
+    printf("DILB created successfully \n");
+}
+
+void InitializeSuperBlock()
+{
+    int i = 0;
+    while(i < MAXINODE)
+    {
+        UFDTArr[i].ptrfiletable = NULL;
+        i++;
+    }
+
+    SUPERBLOCKobj.TotalInodes = MAXINODE;
+    SUPERBLOCKobj.FreeInode = MAXINODE;
+}
+
+int CreateFile(char *name, int permission)
+{
+    int  i = 0;
+    PINODE temp = head;
+
+    if((name == NULL) || (permission == 0) || (permission > 3))
+    {
+        return -1;
+    }
+    if(SUPERBLOCKobj.FreeInode == 0)
+    {
+        return -2;
+    }
+
+    (SUPERBLOCKobj.FreeInode)--;
+
+    if(Get_Inode(name) != NULL)
+    {
+        return -3;
+    }
+
+    while(temp != NULL)
+    {
+        if(temp -> FileType == 0)
+        {
+            break;
+        }
+        temp = temp -> next;
+    }
+
+    while(i < 50)
+    {
+        if(UFDTArr[i].ptrfiletable == NULL)
+        {
+            break;
+        }
+        i++;
+    }
+
+    UFDTArr[i].ptrfiletable = (PFILETABLE)malloc(sizeof(FILETABLE));
+
+    UFDTArr[i].ptrfiletable -> count = 1;
+    UFDTArr[i].ptrfiletable -> mode = permission;
+    UFDTArr[i].ptrfiletable -> readoffset = 0;
+    UFDTArr[i].ptrfiletable -> writeoffset = 0;
+
+    UFDTArr[i].ptrfiletable -> ptrinode = temp;
+
+    strcpy(UFDTArr[i].ptrfiletable -> ptrinode -> FileName,name);
+    UFDTArr[i].ptrfiletable -> ptrinode -> FileType = REGULAR;
+    UFDTArr[i].ptrfiletable -> ptrinode -> ReferenceCount = 1;
+    UFDTArr[i].ptrfiletable -> ptrinode -> LinkCount = 1;
+    UFDTArr[i].ptrfiletable -> ptrinode -> FileSize = MAXFILESIZE;
+    UFDTArr[i].ptrfiletable -> ptrinode -> FileActualSize = 0;
+    UFDTArr[i].ptrfiletable -> ptrinode -> permission = permission;
+    UFDTArr[i].ptrfiletable -> ptrinode -> Buffer = (char *)malloc(MAXFILESIZE);
+
+    return i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    return 0;
+}
+
